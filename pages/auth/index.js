193673b063c838108765083a76a4aaf2d594241d
Epinder <epinder.singh@gmail.com>
2021-02-11 17:40:25 +0800

    styling and life cycle in nextJS

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Router from 'next/router';
 
+import User from '../../components/User';
+
 const authIndexPage = () => (
   <div>
     <h1>The Auth Page</h1>
     <button onClick={() => Router.push('/')}>Go to Home</button>
+    <User name="Epinder" age={38} />
   </div>
 );
 
